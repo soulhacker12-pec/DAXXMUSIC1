3864313c034a8e09fa136dc10dbc564470af1e1f
insanekaneki7 <162685351+insanekaneki7@users.noreply.github.com>
2024-03-18 22:34:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DAXXTEAM/DAXXMUSIC",
+    "https://github.com/insanekaneki7/DAXXMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/INSANE_NETWORK")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/insanesociety")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/anime_flamee")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/anime_flamee")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
